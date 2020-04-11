1ef8026c402803d6f588c81ad93924171bb1e083
oyolah <oyolahmustapha2015@gmail.com>
2020-04-11 20:08:34 +0300

    final commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+import { SearchBar, VideoDetail, VideoList } from "./components";
+
+import youtube from "./api/youtube";
+
+class App extends Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+  componentDidMount() {
+    this.handleSubmit("pdf generation with react and node");
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: process.env.REACT_APP_API_KEY,
+        q: searchTerm,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+  render() {
+    const { selectedVideo, videos } = this.state;
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={11}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={7}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={5}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Do you know that this site is built by Oyolah ?</h1>
+      <h4>Well Yes he did.</h4>
+    </div>
+  );
+};
+
+export default Footer;
